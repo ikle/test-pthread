7d931df31bb663c2ed7cf386382893b91a92e201
Alexei A. Smekalkine <ikle@ikle.ru>
2014-08-15 20:46:23 +0400

    mail-box-test: add mail-box test/sample

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-TARGETS = cond mail-box.o
+TARGETS = cond mail-box-test
 CFLAGS += -pthread
 
 all: $(TARGETS)
@@ -11,3 +11,5 @@ PREFIX ?= /usr/local
 install: $(TARGETS)
 	install -D -d $(DESTDIR)/$(PREFIX)/bin
 	install -s -m 0755 $^ $(DESTDIR)/$(PREFIX)/bin
+
+mail-box-test: mail-box.o
---
+++ mail-box-test.c
@@ -0,0 +1,116 @@
+#include <errno.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <time.h>
+#include <unistd.h>
+#include "mail-box.h"
+
+#define ARRAY_SIZE(a)  (sizeof(a)/sizeof((a)[0]))
+
+/* Thread context */
+typedef ptrdiff_t tid;
+
+#define ctx(id)  ((id) + NULL)
+#define tid_of(ctx)  ((ctx) - NULL)
+
+struct message {
+	tid src;
+	int id;
+};
+
+static struct mail_box *mb;
+
+static void do_fake_work (void)
+{
+	usleep (random () * (0.5e6 / RAND_MAX) + 0.3e6);
+}
+
+static void *worker (void *ctx)
+{
+	int i;
+	struct message *m;
+
+	for (errno = 0, i = 0; i < 10; ++i) {
+		if ((m = malloc (sizeof (*m))) == NULL)
+			break;
+
+		m->src = tid_of (ctx);
+		m->id = i;
+		if (mail_box_put (mb, m) != 0)
+			break;
+
+		do_fake_work ();
+	}
+
+	if (errno != 0)
+		fprintf (stderr, "worker %td: error: %s\n", tid_of (ctx),
+			 strerror (errno));
+	return NULL;
+}
+
+static void *watcher (void *ctx)
+{
+	struct message *m;
+
+	for (errno = 0;;) {
+		if ((m = mail_box_get (mb)) == NULL)
+			break;
+
+		if (m->id < 0) {
+			free (m);
+			break;
+		}
+
+		printf ("message from %td, id = %d\n", m->src, m->id);
+		free (m);
+	}
+
+	if (errno != 0)
+		fprintf (stderr, "watcher %td: error: %s\n", tid_of (ctx),
+			 strerror (errno));
+	return NULL;
+}
+
+int main (int argc, char *argv[])
+{
+	pthread_attr_t attr;
+	pthread_t threads[3];
+	int i;
+	struct message *m;
+
+	srandom (time (NULL));
+
+	if ((mb = mail_box_create ()) == NULL) {
+		perror ("mail_box");
+		return 1;
+	}
+
+	pthread_attr_init (&attr);
+	pthread_attr_setdetachstate (&attr, PTHREAD_CREATE_JOINABLE);
+
+	pthread_create (&threads[0], &attr, watcher, ctx (1));
+	pthread_create (&threads[1], &attr, worker,  ctx (2));
+	pthread_create (&threads[2], &attr, worker,  ctx (3));
+
+	pthread_attr_destroy (&attr);
+
+	/* Wait for all worker threads to complete */
+	for (i = 1; i < ARRAY_SIZE (threads); ++i)
+		pthread_join (threads[i], NULL);
+
+	if ((m = malloc (sizeof (*m))) == NULL) {
+		perror ("main");
+		exit (1);
+	}
+
+	m->src = 0;
+	m->id = -1;
+	mail_box_put (mb, m);
+	pthread_join (threads[0], NULL);
+
+	mail_box_destroy (mb);
+	return 0;
+}
